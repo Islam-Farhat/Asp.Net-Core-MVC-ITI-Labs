ec186954501c359f3d08f522282f1ce4d43e7964
Your Name <islamfarhat100@gmail.com>
2023-03-14 11:17:12 +0200

    MVC lab1 with bouns

---
+++ lab1/lab1/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using lab1.Models;
+
+namespace lab1.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = ProductData.getAllProducts();
+            return View("Index",products);
+        }
+        public IActionResult Details(int id)
+        {
+            var product = ProductData.getProductByID(id);
+            return View("Details",product);
+        }
+    }
+}
