dacfbb2668d8bd5ed069f5258697be97a6e8e9fc
Your Name <islamfarhat100@gmail.com>
2023-03-15 16:18:18 +0200

    MVC lab5 with bouns

---
+++ ITI-Project-MVC/ITI-Project-MVC/Controllers/CourseController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ITI_Project_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+using ITI_Project_MVC.ViewModel;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ITI_Project_MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        ITIContext context = new ITIContext();
+        public IActionResult Index()
+        {
+            var result = context.Courses.Include(s => s.Department).ToList();
+            return View(result);
+        }
+
+        [HttpGet]
+        public IActionResult AddCourse()
+        {
+            ViewBag.deptList = context.Departments.ToList();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCourse(Course newCourse)
+        {
+            if (newCourse.Name != null && newCourse.Degree != null && newCourse.MinDegree != null)
+            {
+                context.Courses.Add(newCourse);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult EditCourse(int id)
+        {
+            ViewBag.deptList = context.Departments.ToList();
+
+            var result = context.Courses.FirstOrDefault(x => x.Id == id);
+
+            return View(result);
+        }
+        [HttpPost]
+        public IActionResult EditCourse([FromRoute] int id, Course newCourse)
+        {
+            ViewBag.deptList = context.Departments.ToList();
+            if (newCourse.Name != null && newCourse.Degree != null)
+            {
+                context.Courses.Update(newCourse);
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(newCourse);
+        }
+
+        public IActionResult DeleteCourse(int id)
+        {
+            var result = context.Courses.FirstOrDefault(x => x.Id == id);
+
+            context.Courses.Remove(result);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
